1e08dc8f9ed93199eda19954ca51ed6334f58a90
thiagosilva89 <thiago.carvalhosilva@hotmail.com>
2022-11-08 19:45:10 -0300

    Create 014 - Lista Exercicios.py

---
+++ IAL002-Alg.Lóg.Progr/00 - Programas/014 - Lista Exercicios.py
@@ -0,0 +1,88 @@
+#print ('x', end = ' ')
+#print ('x', end = ' ')
+
+#print (3 == 3.0)
+#print (1//3)
+#print (3 == '3')
+#print ('x' != 'x')
+#print (2/1)
+#print (2//1)
+#print (not False)
+#print (not True)
+#print (not 0)
+
+#a = 5
+#b = a + 7
+#a = 10
+#print (b)
+
+#print (True and (False or not True))
+
+""" print (type(0))
+
+print (type(0.0))
+
+print (type(3.14))
+
+print (type('Py'))
+
+print (type(True))
+
+print (type(1/2))
+
+print (type(1//2))
+
+print (type(2//1))
+
+print (type(3**3))
+
+print (type(0==0))
+
+print (type(3<0))
+
+print (type(3!=3)) """
+
+#print (type(str(int(3.14159265358979))))
+
+""" a = 20
+print (15-(a-15), end = ' ')
+a = 10
+print (15-(a-15), end = ' ') """
+
+""" a = 12.75
+print (a - int(a), end = ' ')
+a = int((a - int(a))*100)
+print (a)
+
+a = 3
+b = 4
+a = a + b
+b = a - b
+a = a - b
+print (a, b) """
+
+#print (3 % 2)
+
+#print (0 % 2)
+
+#print (123%356254)
+
+#print (type([1, 2]))
+
+#print (type({1:2}))
+
+#print (type([]))
+
+""" a = 'abacate'
+print ('e' in a, 'x' in a, end = ' ')
+print ('ate' in a, end = ' ')
+print ('' in a, end = ' ')
+print ('eta' in a, end = ' ')
+print ('eta' not in a) """
+
+a = '0123456789'
+print (a[0], a[3], a[-1], end = ' ')
+print (a[0:3], a[3:6], a[6:9], end = ' ')
+print (a[:3], a[7:], end = ' ')
+print (a[:9:2], end = ' ')
+print (a[::-1])
